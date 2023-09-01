ddf1f2a6a88402efc95260660e06d99a74c3ba97
Stdcop89dv <62072080+Stdcop89dv@users.noreply.github.com>
2023-09-01 11:40:46 -0500

    retiro id ricardo reyna benavente

--- buscar_cadena.py
+++ buscar_cadena.py
@@ -1,24 +1,39 @@
 import os
+import chardet
 
 def buscar_cadena_en_archivos(carpeta, cadena):
-    archivos_encontrados = []
+    archivos_encontrados = {}
     for nombre_archivo in os.listdir(carpeta):
         ruta_archivo = os.path.join(carpeta, nombre_archivo)
         if os.path.isfile(ruta_archivo):
-            with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
-                contenido = archivo.read()
-                if cadena in contenido:
-                    archivos_encontrados.append(nombre_archivo)
+            try:
+                # Detectar la codificación del archivo
+                with open(ruta_archivo, 'rb') as archivo:
+                    resultado = chardet.detect(archivo.read())
+                    codificacion = resultado['encoding']
+
+                # Leer el archivo con la codificación detectada
+                with open(ruta_archivo, 'r', encoding=codificacion) as archivo:
+                    lineas = archivo.readlines()
+                    lineas_coincidentes = []
+                    for i, linea in enumerate(lineas):
+                        if cadena.lower() in linea.lower():
+                            lineas_coincidentes.append(str(i))
+                    if lineas_coincidentes:
+                        archivos_encontrados[nombre_archivo] = lineas_coincidentes
+            except Exception as e:
+                print(f"Error al leer el archivo: {nombre_archivo}. Ignorando el archivo. Detalles: {str(e)}")
     return archivos_encontrados
 
 if __name__ == '__main__':
-    carpeta = input("Introduce la ruta de la carpeta a buscar: ")
+    carpeta_actual = os.path.dirname(os.path.abspath(__file__))
     cadena_buscar = input("Introduce la cadena a buscar en los archivos: ")
-    archivos_encontrados = buscar_cadena_en_archivos(carpeta, cadena_buscar)
+    archivos_encontrados = buscar_cadena_en_archivos(carpeta_actual, cadena_buscar)
 
     if archivos_encontrados:
-        print("Archivos que contienen la cadena:")
-        for archivo in archivos_encontrados:
-            print(archivo)
+        print("Archivos que contienen la cadena o parte de ella:")
+        for archivo, lineas in archivos_encontrados.items():
+            lineas_encontradas = ",".join(lineas)
+            print(f"{archivo}: {lineas_encontradas}")
     else:
         print("No se encontró la cadena en ningún archivo.")
---
+++ run_init.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\Users\Javier\AppData\Local\Programs\Python\Python310\python.exe" "%~dp0buscar_cadena.py"
+pause
+
+
